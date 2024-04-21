ffbe462c94925317a5928dde629c62e6eb29f18e
saidazimov720 <saidazimovnurbek01gmail.com>
2024-04-21 18:03:17 +0500

    animation object

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const page = document.getElementById('page'),
-      loading = document.getElementById('loading'),
-      slider = document.querySelector('.swiper'),
-      inner1 = document.getElementById('inner-1'),
-      inner2 = document.getElementById('inner-2'),
-      inner3 = document.getElementById('inner-3'),
-      car = document.querySelector('model-viewer'),
-      slideToButtons = document.querySelectorAll('[data-slide-to]'),
-      colorButtons = document.querySelectorAll('[data-color]'),
-      title = document.querySelectorAll('.title'),
-      bgImage = document.querySelector('picture');
+    loading = document.getElementById('loading'),
+    slider = document.querySelector('.swiper'),
+    inner1 = document.getElementById('inner-1'),
+    inner2 = document.getElementById('inner-2'),
+    inner3 = document.getElementById('inner-3'),
+    car = document.querySelector('model-viewer'),
+    slideToButtons = document.querySelectorAll('[data-slide-to]'),
+    colorButtons = document.querySelectorAll('[data-color]'),
+    title = document.querySelectorAll('.title'),
+    bgImage = document.querySelector('picture');
+    
+const innerAnimationActive = {
+    duration: 1,
+    delay: 0.5,
+    ease: Power4.easeOut,
+    autoAlpha: 1,
+    yPercent: 0,
+};
