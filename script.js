87c80114b0bcbea31fbdbb43185cb391fe7b4ebb
saidazimov720 <saidazimovnurbek01gmail.com>
2024-04-22 17:36:49 +0500

    slider title

--- script.js
+++ script.js
@@ -38,4 +38,33 @@ const swiper = new Swiper(slider, {
     keyboard: {
       enabled: true,
     },
+    on: {
+        init: () => {
+          /* SLIDER & TITLE FADE IN */
+          gsap.to(slider, {
+            duration: 1,
+            ease: Power4.easeOut,
+            autoAlpha: 1,
+          });
+          gsap.to(title, innerAnimationActive);
+
+          title.forEach(function (e, i) {
+            let row_width = e.getBoundingClientRect().width;
+            let row_item_width = e.children[0].getBoundingClientRect().width;
+            let offset = ((2 * row_item_width) / row_width) * 100 * -1;
+            let duration = 30 * (i + 1);
+    
+            gsap.set(e, {
+              xPercent: 0
+            });
+    
+            gsap.to(e, {
+              duration: duration,
+              ease: "none",
+              xPercent: offset,
+              repeat: -1
+            });
+          });
+        }
+      },
 })
